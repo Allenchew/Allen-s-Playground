14bb78b6ac467a37a3d27291de54a83988e4a77b
Allen <csoonzhi69@yahoo.com>
2022-05-25 17:45:55 +0800

    Set profile picture, added side bar for project list out

--- src/index.js
+++ src/index.js
@@ -7,6 +7,30 @@ var app;
 
 InitializeApp(app);
 
-var backgroundProfile = document.getElementById("profilePicture");
-backgroundProfile.src = profilePicture;
+var backgroundProfile = document.getElementsByClassName("profilePicture");
+backgroundProfile[0].src = profilePicture;
+backgroundProfile[0].style.opacity = "100%";
+backgroundProfile[0].style.transition = "1s";
+
+setTimeout(typeText, 1000);
+
+var nameField = document.getElementById("name");
+var positionField = document.getElementById("position");
+
+var nameText = "Allen Chew";
+var nameTextIndex = 0;
+
+function typeText()
+{
+    if(nameTextIndex< nameText.length)
+    {
+        nameField.innerHTML += nameText.charAt(nameTextIndex);
+        nameTextIndex++;
+        setTimeout(typeText,50);
+    }else{
+        positionField.style.height = "40px";
+        positionField.style.transition = "0.5s";
+    }
+
+}
 
