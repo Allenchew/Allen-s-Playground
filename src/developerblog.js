9e6ec0dce6198a7b30c3ed3add2ee3f5f94d8fb3
Allen <csoonzhi69@yahoo.com>
2022-06-07 17:40:46 +0800

    added frame for post, added code block for post,need to fix bugs

--- src/developerblog.js
+++ src/developerblog.js
@@ -12,6 +12,15 @@ const auth = getAuth();
 var loginButton = document.getElementById("loginbutton");
 var contentContainer = document.getElementsByClassName("contentcontainer");
 
+var textIndex = 0;
+var paragraphIndex = 0;
+var notOwner = "You Are Not Owner";
+var getAllParagraph = document.getElementsByTagName("p");
+
+var codeBlockHead = "<br><pre class='line-numbers'><code>";
+var codeBlockTail = "</code></pre><br>";
+
+
 onAuthStateChanged(auth,(user)=>{
   loadPosts();
   if(user){
@@ -21,105 +30,9 @@ onAuthStateChanged(auth,(user)=>{
   }
 });
 
-function checkLoginFailed()
-{
-  var params = window.location.href.split("?");
-  if(params.length > 1)
-  {
-    youAreNotOwner();
-  }
-}
-
-var textIndex = 0;
-var paragraphIndex = 0;
-var notOwner = "You Are Not Owner";
-var getAllParagraph = document.getElementsByTagName("p");
-
-function youAreNotOwner()
-{
-  textIndex = 0;
-  getAllParagraph[paragraphIndex].innerHTML = "";
-  typeText();
-}
-
-function typeText()
-{
-  if(textIndex < notOwner.length)
-  {
-    getAllParagraph[paragraphIndex].innerHTML += notOwner.charAt(textIndex);
-    textIndex++;
-    setTimeout(typeText,50);
-  }else if(paragraphIndex < getAllParagraph.length-1){
-    paragraphIndex++;
-    getAllParagraph[paragraphIndex].innerHTML = "";
-    textIndex = 0;
-    setTimeout(typeText,50);
-  }
-}
-
-function loadOwnerFunction()
-{
-  var composeContainer = document.getElementById("composecontainer");
-  var composeTitle = document.createElement("input");
-  var composeTextfield = document.createElement("textarea");
-  var composeButton = document.createElement("Button");
-  composeContainer.appendChild(composeTitle);
-  composeContainer.appendChild(composeTextfield);
-  composeContainer.appendChild(composeButton);
-  composeTitle.setAttribute("type","text");
-  composeTitle.className = "composetitle";
-  composeTitle.setAttribute("placeholder","Title");
-  composeTextfield.className = "composetextfield";
-  composeButton.innerHTML = "Compose";
-  composeButton.className = "composebutton";
-  const todayDate = new Date();
-  var todayDatetoString = todayDate.getDate()+"-"+(todayDate.getMonth()+1)+"-"+todayDate.getFullYear();
-  
-  composeButton.addEventListener("click",function(){
-      if(composeTitle.value != "" && composeTextfield.value != ""){
-        var convertedComposeContent = composeTextfield.value.replace(/\n\r?/g, '<br />');
-        var postData = {
-            "title":composeTitle.value,
-            "date":todayDatetoString,
-            "content":convertedComposeContent
-        }
-        uploadPost(postData);
-      }else{
-        alert("Please fill in both title and content.");
-      }
-  });
-}
-
-function uploadPost(postData){
-  const postKey = push(child(dbref,"post")).key;
-  set(databaseRef(database,"post/"+postKey),postData).then(()=>{
-      window.location.reload();
-  }).catch((error)=>{
-      alert(error.toString);
-  })
-}
-
-function generatePost(postData)
-{
-  var postDiv = document.createElement("div");
-  postDiv.className = "post";
-  var postTitle = document.createElement("div");
-  postTitle.className = "title";
-  var postContent = document.createElement("div");
-  postContent.className = "content";
-  var titleText = document.createElement("h1");
-  titleText.innerHTML = postData["title"];
-  var dateText = document.createElement("h3");
-  dateText.innerHTML = postData["date"];
-  var contentText = document.createElement("p");
-  contentText.innerHTML = postData["content"];
-  postDiv.appendChild(postTitle);
-  postDiv.appendChild(postContent);
-  postTitle.appendChild(titleText);
-  postTitle.appendChild(dateText);
-  postContent.appendChild(contentText);
-  contentContainer[0].appendChild(postDiv);
-}
+loginButton.addEventListener("click",function(){
+  window.location.assign("login.html");
+});
 
 function loadPosts()
 {
@@ -143,6 +56,81 @@ function loadPosts()
     });
 }
 
+function generatePost(postData)
+{
+  var postDiv = document.createElement("div");
+  postDiv.className = "post";
+  var postTitle = document.createElement("div");
+  postTitle.className = "title";
+  var postContent = document.createElement("div");
+  postContent.className = "content";
+  var titleText = document.createElement("h1");
+  titleText.innerHTML = postData["title"];
+  var dateText = document.createElement("h3");
+  dateText.innerHTML = postData["date"];
+  var contentText = document.createElement("p");
+  processTextContent(contentText,postData["content"]);
+  postDiv.appendChild(postTitle);
+  postDiv.appendChild(postContent);
+  postTitle.appendChild(titleText);
+  postTitle.appendChild(dateText);
+  postContent.appendChild(contentText);
+  contentContainer[0].appendChild(postDiv);
+}
+
+function processTextContent(contentText,postContent)
+{
+    var separatedContent = postContent.split("<br>");
+    var newPre = document.createElement("pre");
+    newPre.className = "line-numbers";
+    var newCodeBlock = document.createElement("code");
+    newPre.appendChild(newCodeBlock);
+    var insertedHead = false;
+   console.log(separatedContent);
+    for(var i =0;i<separatedContent.length;i++)
+    {
+        if(insertedHead)
+        {
+            
+        }else
+        {
+            if(i > 0){
+                if(separatedContent[i-1].trim() == "" && separatedContent[i].search("    ") == 0)
+                {
+                    insertedHead = true;
+                    newCodeBlock.innerHTML += separatedContent[i] + "<br>";
+                }
+            }else{
+                
+            }
+        }
+        
+        
+        if(i>0 && !insertedHead){
+            
+        }else{
+            
+        }
+        
+        if(insertedHead && separatedContent[i].search("    ") != 0)
+        {
+            //contentText.innerHTML += codeBlockTail + separatedContent[i] + "<br>";
+            contentText.innerHTML += "<pre>"+newPre.innerHTML+"</pre><br>" + separatedContent[i] + "<br>";
+            newCodeBlock.innerHTML = "";
+            
+            insertedHead = false;
+        }else if(insertedHead && separatedContent[i].search("    ") == 0){
+            
+            //contentText.innerHTML += separatedContent[i] + codeBlockTail;
+        }
+        else{
+            contentText.innerHTML += separatedContent[i] + "<br>";
+        }
+        
+        
+    }
+}
+
 function loadEmptyPost(){
     var emptyPost = {
         "title":"No Post Yet",
@@ -152,7 +140,80 @@ function loadEmptyPost(){
     generatePost(emptyPost);
 }
 
-loginButton.addEventListener("click",function(){
-  window.location.assign("login.html");
-});
+function loadOwnerFunction()
+{
+  var composeContainer = document.getElementById("composecontainer");
+  var composeTitle = document.createElement("input");
+  var composeTextfield = document.createElement("textarea");
+  var composeButton = document.createElement("Button");
+  composeContainer.appendChild(composeTitle);
+  composeContainer.appendChild(composeTextfield);
+  composeContainer.appendChild(composeButton);
+  composeTitle.setAttribute("type","text");
+  composeTitle.className = "composetitle";
+  composeTitle.setAttribute("placeholder","Title");
+  composeTextfield.className = "composetextfield";
+  composeButton.innerHTML = "Compose";
+  composeButton.className = "composebutton";
+  const todayDate = new Date();
+  var todayDatetoString = todayDate.getDate()+"-"+(todayDate.getMonth()+1)+"-"+todayDate.getFullYear();
+  
+  composeButton.addEventListener("click",function(){
+      if(composeTitle.value != "" && composeTextfield.value != ""){
+        var convertedComposeContent = composeTextfield.value.replace(/\n\r?/g, '<br>');
+        var postData = {
+            "title":composeTitle.value,
+            "date":todayDatetoString,
+            "content":convertedComposeContent
+        }
+        uploadPost(postData);
+      }else{
+        alert("Please fill in both title and content.");
+      }
+  });
+}
+
+function checkLoginFailed()
+{
+  var params = window.location.href.split("?");
+  if(params.length > 1)
+  {
+    youAreNotOwner();
+  }
+}
+
+function youAreNotOwner()
+{
+  textIndex = 0;
+  getAllParagraph[paragraphIndex].innerHTML = "";
+  typeText();
+}
+
+function typeText()
+{
+  if(textIndex < notOwner.length)
+  {
+    getAllParagraph[paragraphIndex].innerHTML += notOwner.charAt(textIndex);
+    textIndex++;
+    setTimeout(typeText,50);
+  }else if(paragraphIndex < getAllParagraph.length-1){
+    paragraphIndex++;
+    getAllParagraph[paragraphIndex].innerHTML = "";
+    textIndex = 0;
+    setTimeout(typeText,50);
+  }
+}
+
+function uploadPost(postData){
+  const postKey = push(child(dbref,"post")).key;
+  set(databaseRef(database,"post/"+postKey),postData).then(()=>{
+      window.location.reload();
+  }).catch((error)=>{
+      alert(error.toString);
+  })
+}
+
+
+
+
 
